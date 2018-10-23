b11db4e4ef28eade96ae6c303aa63b9f41c283a3
Pablo Trinidad <pabloatp0204@gmail.com>
2018-10-22 23:19:02 -0500

    0/1 Knapsack problem solved with a DP algorithm

--- Unit 1/Lecture 2/Dynamic programming/demo.py
+++ Unit 1/Lecture 2/Dynamic programming/demo.py
@@ -38,7 +38,7 @@ class Item(object):
 def max_val(left_to_consider, available):
     """Return the best solution to the 0/1 Knapsack problem.
 
-    Assume l a list of items and a a weight.
+    Assume left_to_consider a list of items and available a weight.
     Return a tuple of the total weight of a solution to the 0/1 knapsack
     problem and the items of that solution.
     """
@@ -69,7 +69,51 @@ def max_val(left_to_consider, available):
     return result
 
 
+def fast_max_val(left_to_consider, available, memo={}):
+    """Return the best solution to the 0/1 Knapsack problem using DP.
+
+    Assume left_to_consider a list of items and available a weight and
+    memo the result of previously results of the recursive calls.
+
+    Return a tuple of the total weight of a solution to the 0/1 knapsack
+    problem and the items of that solution.
+    """
+    if(len(left_to_consider), available) in memo:
+        result = memo[(len(left_to_consider), available)]
+    elif left_to_consider == [] or available == 0:
+        result = (0, ())
+    elif left_to_consider[0].weight > available:
+        result = fast_max_val(
+            left_to_consider=left_to_consider[1:],
+            available=available,
+            memo=memo
+        )
+    else:
+        next_item = left_to_consider[0]
+        with_value, with_items = fast_max_val(
+            left_to_consider=left_to_consider[1:],
+            available=available - next_item.weight,
+            memo=memo
+        )
+        with_value += next_item.value
+
+        without_value, without_items = fast_max_val(
+            left_to_consider=left_to_consider[1:],
+            available=available,
+            memo=memo
+        )
+
+        if with_value > without_value:
+            result = (with_value, with_items + (next_item,))
+        else:
+            result = (without_value, without_items)
+
+    memo[(len(left_to_consider), available)] = result
+    return result
+
+
 def build_large_menu(n, max_val, max_cost):
+    """Return random menu of n items bounded to the given max value and max cost."""
     items = []
     for i in range(n):
         items.append(Item(
@@ -80,9 +124,15 @@ def build_large_menu(n, max_val, max_cost):
     return items
 
 
-menu = build_large_menu(3, 10, 10)
-best_option = max_val(menu, 10)
-for _ in menu:
-    print(_)
+def test_knapsack_problem(items, max_cost, algorithm, name):
+    """Test the knapsack problem solutions by calling given algorithm."""
+    print("\nTesting the {} algorithm with a {}-size problem".format(name, n))
+    best_option = algorithm(items, max_cost)
+    print(best_option)
 
-print(best_option)
+import sys
+sys.setrecursionlimit(2000)
+for n in (5, 10, 1024):
+    items = build_large_menu(n, 90, 250)
+    test_knapsack_problem(items, 750, fast_max_val, "DP")
+    # test_knapsack_problem(items, 750, max_val, "left-first, depth-first tree")
